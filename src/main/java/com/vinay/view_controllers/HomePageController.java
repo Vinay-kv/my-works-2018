c97ac2672b71846a42e1788e37b30c78d5c65132
vinay-kv <vinaykv4j@gmail.com>
2018-04-02 00:40:21 +0530

    Added static files

---
+++ src/main/java/com/vinay/view_controllers/HomePageController.java
@@ -0,0 +1,13 @@
+package com.vinay.view_controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomePageController {
+
+	@RequestMapping("/")
+	public String getIndex() {
+		return "index";
+	}
+}
