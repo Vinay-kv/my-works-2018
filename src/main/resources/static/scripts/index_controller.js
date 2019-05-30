3743f01de5884b11c957160241971e5fcf90274a
Vinay-kv <37631785+Vinay-kv@users.noreply.github.com>
2019-05-30 18:00:13 +0530

    Update index_controller.js

--- src/main/resources/static/scripts/index_controller.js
+++ src/main/resources/static/scripts/index_controller.js
@@ -1,3 +1,19 @@
 expdMngtApp.controller('indexController', function($scope) {
 	$scope.heading = "Welcome..";
+	
+	$scope.showAlert = function(ev) {
+    // Appending dialog to document.body to cover sidenav in docs app
+    // Modal dialogs should fully cover application
+    // to prevent interaction outside of dialog
+    $mdDialog.show(
+      $mdDialog.alert()
+        .parent(angular.element(document.querySelector('#popupContainer')))
+        .clickOutsideToClose(true)
+        .title('Status')
+        .textContent('development stopped.')
+        .ariaLabel('Alert Dialog Demo')
+        .ok('Got it!')
+        .targetEvent(ev)
+    );
+  };
 });
